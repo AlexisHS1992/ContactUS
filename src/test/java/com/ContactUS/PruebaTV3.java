18760249e1731ad7490f179f353f75c0cf9abad8
AlexisHS1992 <alexisherrerasalazar9293@gmail.com>
2021-01-15 20:13:31 -0500

    Version 1

---
+++ src/test/java/com/ContactUS/PruebaTV3.java
@@ -0,0 +1,57 @@
+package com.ContactUS;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class PruebaTV3 {
+
+	private WebDriver driver;
+
+	@Before
+
+	public void setUp() {
+
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://automationpractice.com/index.php");
+	}
+
+	@Test
+
+	public void testContactUS() {
+
+		driver.findElement(By.id("contact-link")).click();
+		WebElement SelSubject = driver.findElement(By.name("id_contact"));
+		Select selectSubject = new Select(SelSubject);
+		selectSubject.selectByValue("2");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String pageHeading = driver.findElement(By.className("page-heading bottom-indent")).getText();
+		System.out.println("Page Heading: " + pageHeading);
+
+		
+		driver.findElement(By.id("email")).sendKeys("alexisherrerasalazar9293@gmail.com");
+		driver.findElement(By.id("id_order")).sendKeys("DAASAMXBK");
+		driver.findElement(By.id("message")).sendKeys("I would like to knows how can I buy my product with cash");
+		driver.findElement(By.id("submitMessage")).click();
+
+	}
+
+	@After
+	private void tearDown() {
+
+		driver.quit();
+	}
+
+}
